a52fa09083e868d4b74dcd341fb4d7be331b8860
Katie Cartagena <katiecartagena@hotmail.com>
2020-06-12 14:41:52 -0500

    Module2 Final Check

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -23,3 +23,11 @@ def main():
     myBudgetList = BudgetList(1200)
     expenses = Expense.Expenses()
     expenses.read_expenses('data/spending_data.csv')
+    
+    for expense in expenses.list:
+        myBudgetList.append(expense.amount)
+
+    print('The count of all expenses: ' + str(len(myBudgetList)))
+
+if __name__ == "__main__":
+    main()
