88a53a44dd09cdb1ccc8084370dbb813a959050c
Katie Cartagena <katiecartagena@hotmail.com>
2020-06-13 21:42:51 -0500

    Module5 Task 2 Check

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -1,5 +1,6 @@
 from . import Expense
 import matplotlib.pyplot as plt
+import timeit
 
 def main():
     expenses = Expense.Expenses()
@@ -13,7 +14,8 @@ def main():
     for a,b in zip(divided_for_loop, divided_set_comp):
         if not (a.issubset(b) and b.issubset(a)) :
             print('Sets are NOT equal by subset test')
-
+             
+    timeit.timeit(stmt= "pass", setup=''' ''', number=100000, globals=globals())
 
 if __name__ == "__main__":
     main()
